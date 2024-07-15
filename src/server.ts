00572d7904355239d80978bfb46c28ac1d8953ae
Guilherme Valencia <guilhermevalencia@gmail.com>
2024-07-15 15:08:39 -0400

    adicionando rotas

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,17 @@ import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod
 import { createTrip } from "./routes/create-trip"
 import { confirmTrip } from "./routes/confirm-trip"
 import { confirmParticipants } from "./routes/confirm-participants"
+import { createActivity } from "./routes/create-activity"
+import { getActivities } from "./routes/get-activities"
+import { createLink } from "./routes/create-link"
+import { getLinks } from "./routes/get-links"
+import { getParticipants } from "./routes/get-participants"
+import { createInvite } from "./routes/create-invite"
+import { updateTrip } from "./routes/update-trip"
+import { getTripDetails } from "./routes/get-trip-details"
+import { getParticipant } from "./routes/get-participant"
+import { errorHandler } from "./error-handler"
+import { env } from "./env"
 
 const app = fastify()
 
@@ -14,10 +25,21 @@ app.register(cors, {
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
+app.setErrorHandler(errorHandler)
+
 app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipants)
+app.register(createActivity)
+app.register(createLink)
+app.register(getActivities)
+app.register(getLinks)
+app.register(getParticipants)
+app.register(createInvite)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
     console.log('Server running!')
 })
